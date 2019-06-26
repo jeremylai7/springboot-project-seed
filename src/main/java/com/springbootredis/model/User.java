143c2031cafca8d5aa2514e174ab1417f37fa7f1
jeremylai7 <104902492@qq.com>
2019-06-27 00:01:37 +0800

    通用mapper整合枚举

--- src/main/java/com/springbootredis/model/User.java
+++ src/main/java/com/springbootredis/model/User.java
@@ -1,9 +1,11 @@
 package com.springbootredis.model;
 
+import com.springbootredis.config.handle.EnumHandle;
 import com.springbootredis.model.enums.UserType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import tk.mybatis.mapper.annotation.ColumnType;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
