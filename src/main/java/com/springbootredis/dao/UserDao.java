80fb898233b9c35ce0a0b7eaf66270bdde23b19c
jeremylai7 <1049024592@qq.com>
2020-07-16 16:57:07 +0800

    添加user dao文件

---
+++ src/main/java/com/springbootredis/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.springbootredis.dao;
+
+import com.springbootredis.model.User;
+import com.springbootredis.util.MyMapper;
+
+/**
+ * @Author: laizc
+ * @Date: Created in  2020-07-16
+ * @desc:
+ */
+public interface UserDao extends MyMapper<User>{
+}
