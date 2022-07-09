e9e76a732c17bfd7b98e05822c9c966fe4db798e
jeremylai7 <1049024592@qq.com>
2022-07-09 22:55:44 +0800

    添加 MapperScan 扫描 解决 MyMapper not found

--- demo-admin/src/main/java/com/jeremy/Application.java
+++ demo-admin/src/main/java/com/jeremy/Application.java
@@ -3,8 +3,10 @@ package com.jeremy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.jeremy.data.**.dao")
 public class Application {
 
 	public static void main(String[] args) {
